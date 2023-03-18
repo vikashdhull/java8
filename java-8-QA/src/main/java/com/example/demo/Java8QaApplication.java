0df4c93ab7fc64c070ef20f4f2d11191ef34aa21
vikashdhull <vikashdhull85@gmail.com>
2023-03-19 02:33:00 +0530

    first commit for java 8 project

---
+++ java-8-QA/src/main/java/com/example/demo/Java8QaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8QaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8QaApplication.class, args);
+	}
+
+}
